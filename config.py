bc3e7316e5d02ad3ce80a130cf6d200e365388d2
jiadai <yayamayajd@gamil.com>
2025-03-09 19:53:15 +0100

    try to solve pytest could not find the dburl

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL","sqlite:///:memory:")
     SQLALCHEMY_TRACK_MODIFICATIONS = False  
     SQLALCHEMY_ECHO = False  
     SQLALCHEMY_COMMIT_ON_TEARDOWN = False  
